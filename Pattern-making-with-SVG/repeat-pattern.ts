dc93193133a94a4c04654e4dc87b097c3451c383
Yadira-Canche-Hernandez <yadira.19070010@itsmotul.edu.mx>
2023-02-09 03:58:58 -0600

    Paso 2

--- Pattern-making-with-SVG/repeat-pattern.ts
+++ Pattern-making-with-SVG/repeat-pattern.ts
@@ -1,21 +1,29 @@
-import type {SVGTemplateResult} from "lit";
+import type { SVGTemplateResult } from "lit";
 
-import {LitElement, html, svg, css} from 'lit';
-import {customElement, property} from 'lit/decorators.js';
+import { LitElement, html, svg } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
 
-
-const themeCSS = css`
-  .background {
-    fill: var(--background-color, #000000);
-  }
-
-  text {
-    fill: var(--font-color, #ffffff);
-    font-size: var(--font-size, 26px);
-    stroke-width: var(--stroke-width, 1.2px);
-    stroke: var(--stroke-color, #eeeeee);
-  }
+const createElement = (chars: string): SVGTemplateResult => svg`
+  <text
+    dominant-baseline="hanging"
+    font-family="monospace"
+    font-size="24px">
+    ${chars}
+  </text>
 `;
+
+@customElement('repeat-pattern')
+export class RepeatPattern extends LitElement {    
+  @property({type: String}) chars = "lit";
+  
+  render() {
+    return html`
+      <svg height="100%" width="100%">
+        ${createElement(this.chars)}
+      </svg>
+    `;
+  }
+}
 
 const svgCSS = css`
   :host {
