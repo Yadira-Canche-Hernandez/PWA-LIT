4248bbf04ce36cd5142d6cebbba303198466ffc0
Yadira-Canche-Hernandez <yadira.19070010@itsmotul.edu.mx>
2023-02-09 00:12:24 -0600

    Paso 3

--- Intro to Lit/my-element.ts
+++ Intro to Lit/my-element.ts
@@ -1,13 +1,17 @@
+//paso 3 propiedades y expresiones
 import {LitElement, html} from 'lit';
-import {customElement} from 'lit/decorators.js';
+import {customElement, property} from 'lit/decorators.js';
 
-//definiendo un componente
 @customElement('my-element')
 export class MyElement extends LitElement {
-  //metodo que pertenece a la clase
+  //propiedad
+  @property()
+  message: string = 'Hello again.';
+
   render() {
+    //agrega
     return html`
-      <p>Hello world! From my-element.</p>
+      <p>${this.message}</p>
     `;
   }
 }
